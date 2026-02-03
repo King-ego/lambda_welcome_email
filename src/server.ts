5ae8c07d63505caccd04364d55cfc45fbaf899ec
Diego <diegosantos4957@gmail.com>
2026-02-03 20:04:07 -0300

    feat: integrate serverless-express for AWS Lambda compatibility

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
+import serverlessExpress from '@vendia/serverless-express';
 
 const app = express();
-const PORT = process.env.PORT || 7747;
+/*const PORT = process.env.PORT || 7747;*/
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,9 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+/*
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-});
+});*/
+
+export const handler = serverlessExpress({ app });
