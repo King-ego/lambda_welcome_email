c8b57fc6116052412b532e56a7c479925dad2e17
Diego <diegosantos4957@gmail.com>
2026-02-04 18:22:24 -0300

    feat: update event logging to display event body in handler function

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import {APIGatewayProxyEvent} from "aws-lambda";
 
+interface EventBody {
+    email: string;
+    name: string;
+}
+
 export const handler = async (event: APIGatewayProxyEvent) => {
-   console.log(`Received event: ${JSON.stringify(event, null, 2)}`);
+   console.log(`Received event: ${JSON.stringify(event.body, null, 2)}`);
     return  {
         statusCode: 200,
         body: JSON.stringify({event}),
