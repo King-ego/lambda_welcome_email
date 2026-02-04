15ee1e2afa22bf8e85f99d8cd927308bea27dd3d
Diego <diegosantos4957@gmail.com>
2026-02-04 18:12:30 -0300

    feat: add logging for received event in handler function

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 export const handler = async (event: Event) => {
+   console.log(`Received event: ${JSON.stringify(event, null, 2)}`);
     return  {
         statusCode: 200,
         body: JSON.stringify({event}),
